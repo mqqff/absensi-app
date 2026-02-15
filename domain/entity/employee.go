6febe6d46dd3e2339d073e3ba50e4e3c220c388d
Atha Tsaqif <athatsaaqif@gmail.com>
2026-02-15 11:42:02 +0700

    refactor: update employee struct to use nullable enums for position and department

--- domain/entity/employee.go
+++ domain/entity/employee.go
@@ -9,17 +9,17 @@ import (
 )
 
 type Employee struct {
-	ID         uuid.UUID                   `db:"id" json:"id"`
-	Name       string                      `db:"name" json:"name"`
-	Email      string                      `db:"email" json:"email"`
-	Phone      string                      `db:"phone" json:"phone"`
-	Position   enums.EmployeePositionIdx   `db:"position" json:"position"`
-	Department enums.EmployeeDepartmentIdx `db:"department" json:"department"`
-	Salary     float64                     `db:"salary" json:"salary"`
-	Address    string                      `db:"address" json:"address"`
-	Password   string                      `db:"password" json:"-"`
-	Status     enums.EmployeeStatus        `db:"status" json:"status"`
-	CreatedAt  time.Time                   `db:"created_at" json:"created_at"`
-	UpdatedAt  time.Time                   `db:"updated_at" json:"updated_at"`
-	DeletedAt  sql.NullTime                `db:"deleted_at" json:"deleted_at"`
+	ID         uuid.UUID                       `db:"id" json:"id"`
+	Name       string                          `db:"name" json:"name"`
+	Email      string                          `db:"email" json:"email"`
+	Phone      string                          `db:"phone" json:"phone"`
+	Position   enums.NullEmployeePositionIdx   `db:"position" json:"position"`
+	Department enums.NullEmployeeDepartmentIdx `db:"department" json:"department"`
+	Salary     float64                         `db:"salary" json:"salary"`
+	Address    string                          `db:"address" json:"address"`
+	Password   string                          `db:"password" json:"-"`
+	Status     enums.EmployeeStatus            `db:"status" json:"status"`
+	CreatedAt  time.Time                       `db:"created_at" json:"created_at"`
+	UpdatedAt  time.Time                       `db:"updated_at" json:"updated_at"`
+	DeletedAt  sql.NullTime                    `db:"deleted_at" json:"deleted_at"`
 }
