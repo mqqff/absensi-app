ecf4fe13a17069a5c10789eed560170b10454673
Atha Tsaqif <athatsaaqif@gmail.com>
2026-02-15 15:15:29 +0700

    feat: add attendance controller with CreateAttendance endpoint

---
+++ internal/app/attendance/controller/attendance_controller.go
@@ -0,0 +1,46 @@
+package controller
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/mqqff/absensi-app/domain/contracts"
+	"github.com/mqqff/absensi-app/domain/dto"
+	"github.com/mqqff/absensi-app/domain/errx"
+	"github.com/mqqff/absensi-app/internal/middlewares"
+	"github.com/mqqff/absensi-app/pkg/helpers/http/response"
+	"github.com/mqqff/absensi-app/pkg/jwt"
+)
+
+type attendanceController struct {
+	attendanceService contracts.AttendanceService
+}
+
+func InitAttendanceController(
+	router fiber.Router,
+	attendanceService contracts.AttendanceService,
+	middleware *middlewares.Middleware,
+) {
+	attendanceCtr := attendanceController{
+		attendanceService: attendanceService,
+	}
+
+	attendanceGroup := router.Group("/attendances", middleware.RequireAuth())
+	attendanceGroup.Post("/", attendanceCtr.CreateAttendance)
+}
+
+func (c *attendanceController) CreateAttendance(ctx *fiber.Ctx) error {
+	claims, ok := ctx.Locals("claims").(jwt.Claims)
+	if !ok {
+		return errx.ErrNoBearerToken
+	}
+
+	req := dto.CreateAttendanceRequest{
+		EmployeeID: claims.EmployeeID,
+	}
+
+	res, err := c.attendanceService.CreateAttendance(ctx.Context(), req)
+	if err != nil {
+		return err
+	}
+
+	return response.SendResponse(ctx, fiber.StatusCreated, res)
+}
