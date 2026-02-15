b53b0a7766d9be607b4975902ebe49b1b2bf1a1c
Atha Tsaqif <athatsaaqif@gmail.com>
2026-02-15 14:17:05 +0700

    feat: add methods for fetching multiple employees and counting employees in repository and service

--- domain/contracts/employee_contracts.go
+++ domain/contracts/employee_contracts.go
@@ -8,6 +8,8 @@ import (
 )
 
 type EmployeeRepository interface {
+	GetEmployees(ctx context.Context, query dto.EmployeesQuery, pagination dto.PaginationRequest) ([]entity.Employee, error)
+	CountEmployees(ctx context.Context, query dto.EmployeesQuery) (int, error)
 	GetEmployeeByID(ctx context.Context, employeeID string) (entity.Employee, error)
 	CreateEmployee(ctx context.Context, employee entity.Employee) error
 	UpdateEmployee(ctx context.Context, employee entity.Employee) error
@@ -15,6 +17,7 @@ type EmployeeRepository interface {
 }
 
 type EmployeeService interface {
+	GetEmployees(ctx context.Context, query dto.GetEmployeesQuery) (dto.GetEmployeesResponse, error)
 	GetEmployee(ctx context.Context, param dto.GetEmployee) (dto.EmployeeResponse, error)
 	CreateEmployee(ctx context.Context, data dto.CreateEmployeeRequest) error
 	UpdateEmployee(ctx context.Context, data dto.UpdateEmployeeRequest) error
