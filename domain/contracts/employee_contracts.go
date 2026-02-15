ae2dd2255cfb5260fda3360123931f69dc997f2b
Atha Tsaqif <athatsaaqif@gmail.com>
2026-02-15 12:33:26 +0700

    feat: update EmployeeService interface to use CreateEmployeeRequest DTO for employee creation

--- domain/contracts/employee_contracts.go
+++ domain/contracts/employee_contracts.go
@@ -3,9 +3,14 @@ package contracts
 import (
 	"context"
 
+	"github.com/mqqff/absensi-app/domain/dto"
 	"github.com/mqqff/absensi-app/domain/entity"
 )
 
 type EmployeeRepository interface {
 	CreateEmployee(ctx context.Context, employee entity.Employee) error
+}
+
+type EmployeeService interface {
+	CreateEmployee(ctx context.Context, data dto.CreateEmployeeRequest) error
 }
