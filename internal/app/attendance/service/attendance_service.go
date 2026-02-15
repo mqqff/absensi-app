fa9cd965f9f4f632a4fede04198bdd8914a1f557
Atha Tsaqif <athatsaaqif@gmail.com>
2026-02-15 15:15:20 +0700

    feat: implement attendance service with CreateAttendance method

---
+++ internal/app/attendance/service/attendance_service.go
@@ -0,0 +1,49 @@
+package service
+
+import (
+	"context"
+	"time"
+
+	"github.com/mqqff/absensi-app/domain/contracts"
+	"github.com/mqqff/absensi-app/domain/dto"
+	"github.com/mqqff/absensi-app/domain/entity"
+	"github.com/mqqff/absensi-app/pkg/uuid"
+	"github.com/mqqff/absensi-app/pkg/validator"
+)
+
+type attendanceService struct {
+	attendanceRepo contracts.AttendanceRepository
+	validator      validator.ValidatorInterface
+	uuid           uuid.UUIDInterface
+}
+
+func NewAttendanceService(
+	attendanceRepo contracts.AttendanceRepository,
+	validator validator.ValidatorInterface,
+	uuid uuid.UUIDInterface,
+) contracts.AttendanceService {
+	return &attendanceService{
+		attendanceRepo, validator, uuid,
+	}
+}
+
+func (s *attendanceService) CreateAttendance(ctx context.Context, data dto.CreateAttendanceRequest) (dto.AttendanceResponse, error) {
+	attendanceID, _ := s.uuid.NewV7()
+
+	attendance := entity.Attendance{
+		ID:         attendanceID,
+		EmployeeID: data.EmployeeID,
+		CheckIn:    time.Now(),
+	}
+
+	err := s.attendanceRepo.CreateAttendance(ctx, attendance)
+	if err != nil {
+		return dto.AttendanceResponse{}, err
+	}
+
+	return dto.AttendanceResponse{
+		ID:         attendance.ID,
+		EmployeeID: attendance.EmployeeID,
+		CheckIn:    attendance.CheckIn,
+	}, nil
+}
