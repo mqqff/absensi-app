fef441345dd2f62fd0cc7eb01370d83f8ecb0bf9
Atha Tsaqif <athatsaaqif@gmail.com>
2026-02-15 13:34:48 +0700

    feat: add employee response DTO and formatting function for API responses

--- domain/dto/employee_dto.go
+++ domain/dto/employee_dto.go
@@ -1,9 +1,35 @@
 package dto
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/google/uuid"
+	"github.com/mqqff/absensi-app/domain/entity"
 	"github.com/mqqff/absensi-app/domain/enums"
 )
+
+type EmployeeResponse struct {
+	ID         string    `json:"id"`
+	Name       string    `json:"name"`
+	Email      string    `json:"email"`
+	Phone      string    `json:"phone"`
+	Position   string    `json:"position"`
+	Department string    `json:"department"`
+	Salary     float64   `json:"salary"`
+	Address    string    `json:"address"`
+	Status     string    `json:"status"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
+
+type GetEmployeeResponse struct {
+	Employees []EmployeeResponse `json:"employees"`
+}
+
+type GetEmployee struct {
+	ID string `param:"id" validate:"required,uuid"`
+}
 
 type CreateEmployeeRequest struct {
 	Name       string                      `json:"name" validate:"required"`
@@ -32,4 +58,31 @@ type UpdateEmployeeRequest struct {
 
 type DeleteEmployeeParam struct {
 	ID string `param:"id" validate:"required,uuid"`
+}
+
+func FormatToEmployeeResponse(employee entity.Employee) EmployeeResponse {
+	fmt.Println(employee)
+	var position string
+	if employee.Position.Valid {
+		position = enums.EmployeePositionMapIdx[employee.Position.EmployeePositionIdx].LongLabel["id"]
+	}
+
+	var department string
+	if employee.Department.Valid {
+		department = enums.EmployeeDepartmentMapIdx[employee.Department.EmployeeDepartmentIdx].LongLabel["id"]
+	}
+
+	return EmployeeResponse{
+		ID:         employee.ID.String(),
+		Name:       employee.Name,
+		Email:      employee.Email,
+		Phone:      employee.Phone,
+		Position:   position,
+		Department: department,
+		Salary:     employee.Salary,
+		Address:    employee.Address,
+		Status:     enums.EmployeeStatusMap[employee.Status],
+		CreatedAt:  employee.CreatedAt,
+		UpdatedAt:  employee.UpdatedAt,
+	}
 }
