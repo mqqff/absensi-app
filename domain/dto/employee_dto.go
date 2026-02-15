83e9d33e1a5960332c43e1b9d7f4d8876c67b24e
Atha Tsaqif <athatsaaqif@gmail.com>
2026-02-15 12:58:54 +0700

    feat: add UpdateEmployeeRequest DTO for employee updates

--- domain/dto/employee_dto.go
+++ domain/dto/employee_dto.go
@@ -1,6 +1,9 @@
 package dto
 
-import "github.com/mqqff/absensi-app/domain/enums"
+import (
+	"github.com/google/uuid"
+	"github.com/mqqff/absensi-app/domain/enums"
+)
 
 type CreateEmployeeRequest struct {
 	Name       string                      `json:"name" validate:"required"`
@@ -8,6 +11,19 @@ type CreateEmployeeRequest struct {
 	Phone      string                      `json:"phone" validate:"required"`
 	Salary     float64                     `json:"salary" validate:"required,gt=0"`
 	Password   string                      `json:"password" validate:"required,min=6"`
+	Address    string                      `json:"address" validate:"required"`
+	Position   enums.EmployeePositionIdx   `json:"position" validate:"required,oneof=1 2 3 4"`
+	Department enums.EmployeeDepartmentIdx `json:"department" validate:"required,oneof=1 2 3 4 5"`
+	Status     enums.EmployeeStatus        `json:"status"`
+}
+
+type UpdateEmployeeRequest struct {
+	ID         uuid.UUID                   `param:"id" validate:"required,uuid"`
+	Name       string                      `json:"name" validate:"required"`
+	Email      string                      `json:"email" validate:"required,email"`
+	Phone      string                      `json:"phone" validate:"required"`
+	Salary     float64                     `json:"salary" validate:"required,gt=0"`
+	Password   string                      `json:"password" validate:"omitempty,min=6"`
 	Address    string                      `json:"address" validate:"required"`
 	Position   enums.EmployeePositionIdx   `json:"position" validate:"required,oneof=1 2 3 4"`
 	Department enums.EmployeeDepartmentIdx `json:"department" validate:"required,oneof=1 2 3 4 5"`
