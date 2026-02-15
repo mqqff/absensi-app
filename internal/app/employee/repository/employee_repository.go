5e46ca47876a252f44540187ca955243f70d79e4
Atha Tsaqif <athatsaaqif@gmail.com>
2026-02-15 14:17:20 +0700

    feat: add filtering and pagination methods for fetching employees in repository

--- internal/app/employee/repository/employee_repository.go
+++ internal/app/employee/repository/employee_repository.go
@@ -3,10 +3,13 @@ package repository
 import (
 	"context"
 	"errors"
+	"fmt"
+	"strings"
 
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jmoiron/sqlx"
 	"github.com/mqqff/absensi-app/domain/contracts"
+	"github.com/mqqff/absensi-app/domain/dto"
 	"github.com/mqqff/absensi-app/domain/entity"
 	"github.com/mqqff/absensi-app/domain/errx"
 	"github.com/mqqff/absensi-app/pkg/helpers/pgerror"
@@ -70,6 +73,88 @@ func (r *employeeRepository) Rollback() error {
 	r.tx = nil
 
 	return nil
+}
+
+func (r *employeeRepository) buildEmployeeFilter(query dto.EmployeesQuery) (string, []interface{}) {
+	baseQuery := `
+		FROM employees
+		WHERE deleted_at IS NULL
+	`
+
+	var (
+		conditions []string
+		args       []interface{}
+	)
+
+	if query.Name != "" {
+		args = append(args, "%"+query.Name+"%")
+		conditions = append(conditions, fmt.Sprintf("name ILIKE $%d", len(args)))
+	}
+
+	if query.Email != "" {
+		args = append(args, "%"+query.Email+"%")
+		conditions = append(conditions, fmt.Sprintf("email ILIKE $%d", len(args)))
+	}
+
+	if query.Position != 0 {
+		args = append(args, query.Position)
+		conditions = append(conditions, fmt.Sprintf("position = $%d", len(args)))
+	}
+
+	if query.Department != 0 {
+		args = append(args, query.Department)
+		conditions = append(conditions, fmt.Sprintf("department = $%d", len(args)))
+	}
+
+	if query.Status != 0 {
+		args = append(args, query.Status)
+		conditions = append(conditions, fmt.Sprintf("status = $%d", len(args)))
+	}
+
+	if len(conditions) > 0 {
+		baseQuery += " AND " + strings.Join(conditions, " AND ")
+	}
+
+	return baseQuery, args
+}
+
+func (r *employeeRepository) GetEmployees(
+	ctx context.Context,
+	query dto.EmployeesQuery,
+	pagination dto.PaginationRequest,
+) ([]entity.Employee, error) {
+
+	var employees []entity.Employee
+
+	baseQuery, args := r.buildEmployeeFilter(query)
+
+	dataQuery := `
+		SELECT id, name, email, phone, position, department, salary, address, status
+	` + baseQuery + fmt.Sprintf(`
+		ORDER BY name ASC
+		LIMIT $%d OFFSET $%d
+	`, len(args)+1, len(args)+2)
+
+	args = append(args, pagination.Limit, pagination.Offset)
+
+	if err := r.conn.SelectContext(ctx, &employees, dataQuery, args...); err != nil {
+		return nil, err
+	}
+
+	return employees, nil
+}
+
+func (r *employeeRepository) CountEmployees(ctx context.Context, query dto.EmployeesQuery) (int, error) {
+	baseQuery, args := r.buildEmployeeFilter(query)
+
+	countQuery := `SELECT COUNT(*) ` + baseQuery
+
+	var total int
+	if err := r.conn.GetContext(ctx, &total, countQuery, args...); err != nil {
+		return 0, err
+	}
+
+	return total, nil
 }
 
 func (r *employeeRepository) GetEmployeeByID(ctx context.Context, employeeID string) (entity.Employee, error) {
